2438b4a481412799e51d41207089504fac2f0df3
unknown <marufakhanom03021994@gmail.com>
2024-11-30 11:22:21 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+function myname(){
+    document.getElementById("myname").innerHTML ="Web Developer"
+}
+function myimage() {
+   document.getElementById('myimage').src='image/Tulip.png'
+  }
+
+function image(){
+    document.getElementById('myimage').src='image/Lotus.png'
+}
+
+function fontstyle(){
+   document.getElementById('fontstyle').style.fontSize="50px";
+}
+
+function hidetext(){
+    document.getElementById('hide').style.display='none'
+}
+function blockelement(){
+    document.getElementById('block-element').style.display='block'
+}
